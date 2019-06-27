ce4b62e3cb5a02f40a76acc4c226e0410b48b19a
Charles Nicholson <charles.nicholson@gmail.com>
2019-06-27 07:57:18 -0400

    rearrange conformance

--- unit_tests/test_conformance.cc
+++ unit_tests/test_conformance.cc
@@ -58,6 +58,16 @@ TEST(conformance, Char) {
         sprintf(fmt, "%%-%dc", precision);
         CheckConformance(fmt, 'A');
     }
+
+    CheckConformance("%+c", 'A');
+    CheckConformance("%+c", 0);
+    CheckConformance("%+c", -1);
+    CheckConformance("% 6c", 'A');
+    CheckConformance("% 6c", 0);
+    CheckConformance("% 6c", -1);
+    CheckConformance("%+4c", 1);
+    CheckConformance("%+4c", 0);
+    CheckConformance("%+4c", -1);
 }
 
 TEST(conformance, Strings) {
@@ -69,6 +79,12 @@ TEST(conformance, Strings) {
 TEST(conformance, UnsignedInt) {
     CheckConformance("%u", 0);
     CheckConformance("%u", UINT_MAX);
+    CheckConformance("%+u", 0);
+    CheckConformance("%+u", 1);
+    CheckConformance("%+4u", 1);
+    CheckConformance("%+4u", 0);
+    CheckConformance("% 6u", 0);
+    CheckConformance("% 6u", 1);
 }
 
 TEST(conformance, Octal) {
@@ -77,12 +93,32 @@ TEST(conformance, Octal) {
     CheckConformance("%o", UINT_MAX);
     CheckConformance("%10o", 1234);
     CheckConformance("%#10o", 1234);
+    CheckConformance("%04o", 1);
+    CheckConformance("%04o", 0);
+    CheckConformance("%+o", 0);
+    CheckConformance("%+o", 1);
+    CheckConformance("%+4o", 1);
+    CheckConformance("%+4o", 0);
+    CheckConformance("% 6o", 0);
+    CheckConformance("% 6o", 1);
 }
 
 TEST(conformance, SignedInt) {
     CheckConformance("%i", INT_MIN);
     CheckConformance("%i", 0);
     CheckConformance("%i", INT_MAX);
+    CheckConformance("%+i", -1);
+    CheckConformance("%+i", 0);
+    CheckConformance("%+i", 1);
+    CheckConformance("% 6i", -1);
+    CheckConformance("% 6i", 0);
+    CheckConformance("% 6i", 1);
+    CheckConformance("%+4i", 1);
+    CheckConformance("%+4i", 0);
+    CheckConformance("%+4i", -1);
+    CheckConformance("%04i", 1);
+    CheckConformance("%04i", 0);
+    CheckConformance("%04i", -1);
     // CheckConformance("%.-123i", 400); xcode libc doesn't ignore negative
 }
 
@@ -96,67 +132,20 @@ TEST(conformance, Hex) {
     CheckConformance("%#x", 0);
     CheckConformance("%10x", 0x1234);
     CheckConformance("%#10x", 0x1234);
+    CheckConformance("%04u", 1);
+    CheckConformance("%04u", 0);
+    CheckConformance("% 6x", 0);
+    CheckConformance("% 6x", 1);
+    CheckConformance("%+x", 0);
+    CheckConformance("%+x", 1);
 }
 
 TEST(conformance, Pointer) {
     // CheckConformance("%p", nullptr); implementation defined
     int x, *p = &x;
     CheckConformance("%p", p);
-    CheckConformance("%10p", p);
-}
-
-TEST(conformance, PrependSign) {
-    CheckConformance("%+%");
-    CheckConformance("%+c", 'A');
-    CheckConformance("%+c", 0);
-    CheckConformance("%+c", -1);
-    CheckConformance("%+i", -1);
-    CheckConformance("%+i", 0);
-    CheckConformance("%+i", 1);
-    CheckConformance("%+u", 0);
-    CheckConformance("%+u", 1);
-    CheckConformance("%+o", 0);
-    CheckConformance("%+o", 1);
-    CheckConformance("%+x", 0);
-    CheckConformance("%+x", 1);
-}
-
-TEST(conformance, PrependSpace) {
-    CheckConformance("% %");
-    CheckConformance("% 6c", 'A');
-    CheckConformance("% 6c", 0);
-    CheckConformance("% 6c", -1);
-    CheckConformance("% 6i", -1);
-    CheckConformance("% 6i", 0);
-    CheckConformance("% 6i", 1);
-    CheckConformance("% 6u", 0);
-    CheckConformance("% 6u", 1);
-    CheckConformance("% 6o", 0);
-    CheckConformance("% 6o", 1);
-    CheckConformance("% 6x", 0);
-    CheckConformance("% 6x", 1);
-}
-
-TEST(conformance, FieldWidthAndSign) {
-    CheckConformance("%+4c", 1);
-    CheckConformance("%+4c", 0);
-    CheckConformance("%+4c", -1);
-    CheckConformance("%+4i", 1);
-    CheckConformance("%+4i", 0);
-    CheckConformance("%+4i", -1);
-    CheckConformance("%+4u", 1);
-    CheckConformance("%+4u", 0);
-    CheckConformance("%+4o", 1);
-    CheckConformance("%+4o", 0);
-}
-
-TEST(conformance, FieldWidthAndZeroPad) {
-    // zero-pad is undefined on char
-    CheckConformance("%04i", 1);
-    CheckConformance("%04i", 0);
-    CheckConformance("%04i", -1);
-    CheckConformance("%04u", 1);
-    CheckConformance("%04u", 0);
-    CheckConformance("%04o", 1);
-    CheckConformance("%04o", 0);
+    CheckConformance("%30p", p);
+    CheckConformance("% 30p", p);
+    // CheckConformance("%030p", p); 0x comes before zero pad
+    // CheckConformance("%.30p", p); 0x comes before precision
 }
