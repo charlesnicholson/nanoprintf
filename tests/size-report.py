213a15001de49f42f4c3c7acb89a0c97c46ff277
Charles Nicholson <charles.nicholson@gmail.com>
2021-12-26 08:42:38 -0500

    add python script for summing nm output

---
+++ tests/size-report.py
@@ -0,0 +1,14 @@
+"""Accumulate size output from nm and print the total."""
+import sys
+
+def main():
+  total = 0
+  for line in sys.stdin:
+    print(line, end='')
+    total += int(line.split()[1], 16)
+
+  print(f'Total size: 0x{total:x} ({total}) bytes')
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
