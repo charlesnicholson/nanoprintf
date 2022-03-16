5414370b83bcb76edffc2c3ea4773c9098807a7d
Charles Nicholson <charles.nicholson@gmail.com>
2022-03-16 09:33:09 -0400

    add preprocessor options for handling snprintf buffer overflow (#195)
    * add preprocessor options for handling snprintf buffer overflow
    
    * add tests
    
    * safety docs
    
    * markdown typo
    
    * markdown typo

---
+++ tests/unit_snprintf_safe_empty.cc
@@ -0,0 +1,34 @@
+#define NANOPRINTF_SNPRINTF_SAFE_EMPTY_STRING_ON_OVERFLOW
+#include "unit_nanoprintf.h"
+
+#include <string>
+#include <iostream>
+
+TEST_CASE("snprintf safety: empty string") {
+  char buf[9];
+  buf[7] = '*';
+  buf[8] = '!';
+
+  SUBCASE("small string") {
+    REQUIRE(npf_snprintf(buf, 8, "abc") == 3);
+    REQUIRE(std::string{buf} == "abc");
+  }
+
+  SUBCASE("exact fit string") {
+    REQUIRE(npf_snprintf(buf, 8, "1234567") == 7);
+    REQUIRE(buf[7] == '\0');
+    REQUIRE(std::string{buf} == "1234567");
+  }
+
+  SUBCASE("if the null terminator doesn't fit, the string is empty") {
+    REQUIRE(npf_snprintf(buf, 8, "12345678") == 8);
+    REQUIRE(buf[0] == '\0');
+    REQUIRE(buf[8] == '!');
+  }
+
+  SUBCASE("if the string contents are too long, the string is empty") {
+    REQUIRE(npf_snprintf(buf, 8, "123456789") == 9);
+    REQUIRE(buf[0] == '\0');
+    REQUIRE(buf[8] == '!');
+  }
+}
