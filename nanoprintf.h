20a0b2c1c6106e8ba7c5619bd2fdc46715e24d81
Charles Nicholson <charles.nicholson@gmail.com>
2022-03-27 10:23:33 -0400

    pointers are smaller

--- nanoprintf.h
+++ nanoprintf.h
@@ -502,20 +502,20 @@ int npf_parse_format_spec(char const *format, npf_format_spec_t *out_spec) {
 }
 
 int npf_itoa_rev(char *buf, npf_int_t i) {
-  int n = 0;
+  char *dst = buf;
   int const sign = (i >= 0) ? 1 : -1;
-  do { buf[n++] = (char)('0' + (sign * (i % 10))); i /= 10; } while (i);
-  return n;
+  do { *dst++ = (char)('0' + (sign * (i % 10))); i /= 10; } while (i);
+  return (int)(dst - buf);
 }
 
 int npf_utoa_rev(char *buf, npf_uint_t i, unsigned base, unsigned case_adj) {
-  int n = 0;
+  char *dst = buf;
   do {
     unsigned const d = (unsigned)(i % base);
-    buf[n++] = (char)((d < 10) ? ('0' + d) : ('A' + case_adj + (d - 10)));
+    *dst++ = (char)((d < 10) ? ('0' + d) : ('A' + case_adj + (d - 10)));
     i /= base;
   } while (i);
-  return n;
+  return (int)(dst - buf);
 }
 
 #if NANOPRINTF_USE_FLOAT_FORMAT_SPECIFIERS == 1
