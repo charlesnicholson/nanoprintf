860c2f4488c4e0157b12dc7adc57bd27d4dbccd7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 13:34:03 +0000

    Adding tarfile member sanitization to extractall()

--- build.py
+++ build.py
@@ -87,7 +87,26 @@ def get_cmake(download, verbose):
                 cmake_local_tgz,
                 verbose)
         with tarfile.open(cmake_local_tgz, 'r') as tar:
-            tar.extractall(path=cmake_local_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=cmake_local_dir)
 
     return cmake_local_exe
 
