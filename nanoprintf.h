c2d00b27dfa59f2418bb5bb6941394f2346fb0e9
Charles Nicholson <charles.nicholson@gmail.com>
2022-11-23 23:54:12 -0500

    remove temporary variable (#235)

--- nanoprintf.h
+++ nanoprintf.h
@@ -762,8 +762,9 @@ int npf_vpprintf(npf_putc pc, void *pc_ctx, char const *format, va_list args) {
       case NPF_FMT_SPEC_CONV_STRING: {
         cbuf = va_arg(args, char *);
 #if NANOPRINTF_USE_PRECISION_FORMAT_SPECIFIERS == 1
-        int const n = (fs.prec_opt == NPF_FMT_SPEC_OPT_NONE) ? -1 : fs.prec;
-        for (char const *s = cbuf; *s && ((n == -1) || (cbuf_len < n)); ++s, ++cbuf_len);
+        for (char const *s = cbuf;
+             *s && ((fs.prec_opt == NPF_FMT_SPEC_OPT_NONE) || (cbuf_len < fs.prec));
+             ++s, ++cbuf_len);
 #else
         for (char const *s = cbuf; *s; ++s, ++cbuf_len); // strlen
 #endif
