952198682c98110a2ae730002b120d450c9955bc
Charles Nicholson <charles.nicholson@gmail.com>
2022-12-03 15:45:14 -0500

    shut up msvc about doctest

--- tests/doctest_main.cc
+++ tests/doctest_main.cc
@@ -1,5 +1,6 @@
 #ifdef _MSC_VER
   #pragma warning(disable:5246) // initialization of subobject needs braces
+  #pragma warning(disable:5262) // implicit switch fall-through
 #endif
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
