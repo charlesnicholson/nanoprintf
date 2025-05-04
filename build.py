8ef78aafcaef4913f5bfc2f79df90e7f41b0b3f7
Charles Nicholson <charles.nicholson@gmail.com>
2025-05-03 20:23:06 -0400

    fix download links, add type annotations, format

--- build.py
+++ build.py
@@ -11,175 +11,187 @@ import tarfile
 import urllib.request
 import zipfile
 
-SCRIPT_PATH = pathlib.Path(__file__).resolve().parent
+_SCRIPT_PATH = pathlib.Path(__file__).resolve().parent
+_NINJA_URL = "https://github.com/ninja-build/ninja/releases/download/v1.12.1/{}"
+_CMAKE_VERSION = "4.0.1"
+_CMAKE_URL = (
+    "https://github.com/Kitware/CMake/releases/download/"
+    f"v{_CMAKE_VERSION}/cmake-{_CMAKE_VERSION}" + "-{}.{}"
+)
 
-NINJA_URL = 'https://github.com/ninja-build/ninja/releases/download/v1.10.2/{}'
-CMAKE_URL = 'https://cmake.org/files/v3.22/{}'
 
-
-def parse_args():
+def _parse_args() -> argparse.Namespace:
     """Parse command-line arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--cfg',
-        choices=[
-            'Debug',
-            'RelWithDebInfo',
-            'Release'],
-        default='Release',
-        const='Release',
-        nargs='?',
-        help='CMake configuration')
+        "--cfg",
+        choices=["Debug", "RelWithDebInfo", "Release"],
+        default="Release",
+        const="Release",
+        nargs="?",
+        help="CMake configuration",
+    )
     parser.add_argument(
-        '--arch',
+        "--arch",
         type=int,
         choices=(32, 64),
         default=64,
         const=64,
-        nargs='?',
-        help='Target architecture')
+        nargs="?",
+        help="Target architecture",
+    )
     parser.add_argument(
-        '--paland',
-        help='Compile with Paland\'s printf conformance suite',
-        action='store_true')
+        "--paland",
+        help="Compile with Paland's printf conformance suite",
+        action="store_true",
+    )
     parser.add_argument(
-        '--download',
-        help='Download CMake and Ninja, don\'t use local copies',
-        action='store_true')
-    parser.add_argument('--ubsan', action='store_true', help='Clang UB sanitizer')
-    parser.add_argument('--asan', action='store_true', help='Clang addr sanitizer')
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
+        "--download",
+        help="Download CMake and Ninja, don't use local copies",
+        action="store_true",
+    )
+    parser.add_argument("--ubsan", action="store_true", help="Clang UB sanitizer")
+    parser.add_argument("--asan", action="store_true", help="Clang addr sanitizer")
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose")
     return parser.parse_args()
 
 
-def download_file(url, local_path, verbose):
+def download_file(url: str, local_path: pathlib.Path, verbose: bool) -> None:
     """Download a file from url to local_path."""
     if verbose:
-        print(f'Downloading:\n  Remote: {url}\n  Local: {local_path}')
-    with urllib.request.urlopen(url) as rsp, open(local_path, 'wb') as file:
+        print(f"Downloading:\n  Remote: {url}\n  Local: {local_path}")
+    with urllib.request.urlopen(url) as rsp, open(local_path, "wb") as file:
         shutil.copyfileobj(rsp, file)
 
 
-def get_cmake(download, verbose):
+def _get_cmake(download: bool, verbose: bool) -> pathlib.Path:
     """Return the path to system CMake, or download and unpack a local copy."""
     if not download:
-        cmake = shutil.which('cmake')
+        cmake = shutil.which("cmake")
         if cmake:
-            return cmake
+            return pathlib.Path(cmake)
 
     plat = {
-        'darwin': 'macos-universal',
-        'linux': 'linux-x86_64',
-        'win32': 'win64-x86_64'}[
-        sys.platform]
-    cmake_prefix = f'cmake-3.22.2-{plat}'
-    cmake_local_dir = SCRIPT_PATH / 'external' / 'cmake'
-    cmake_file = f'{cmake_prefix}.tar.gz'
+        "darwin": "macos-universal",
+        "linux": "linux-x86_64",
+        "win32": "windows-x86_64",
+    }[sys.platform]
+
+    suffix = "zip" if sys.platform == "win32" else "tar.gz"
+
+    cmake_prefix = f"cmake-{_CMAKE_VERSION}-{plat}"
+    cmake_local_dir = _SCRIPT_PATH / "external/cmake"
+    cmake_file = f"{cmake_prefix}.tar.gz"
     cmake_local_tgz = cmake_local_dir / cmake_file
-    cmake_local_exe = cmake_local_dir / cmake_prefix / \
-        ('CMake.app/Contents' if sys.platform == 'darwin' else '') / 'bin' / 'cmake'
+    cmake_local_exe = (
+        cmake_local_dir
+        / cmake_prefix
+        / ("CMake.app/Contents" if sys.platform == "darwin" else "")
+        / "bin/cmake"
+    )
 
     if not cmake_local_exe.exists():
         if not cmake_local_tgz.exists():
             cmake_local_dir.mkdir(parents=True, exist_ok=True)
-            download_file(
-                CMAKE_URL.format(cmake_file),
-                cmake_local_tgz,
-                verbose)
-        with tarfile.open(cmake_local_tgz, 'r') as tar:
+            download_file(_CMAKE_URL.format(plat, suffix), cmake_local_tgz, verbose)
+
+        with tarfile.open(cmake_local_tgz, "r") as tar:
             for member in tar.getmembers():
                 member_path = pathlib.Path(cmake_local_dir / member.name).resolve()
-                if not cmake_local_dir in member_path.parents:
-                    raise ValueError('Tar file contents move upwards past sandbox root')
+                if cmake_local_dir not in member_path.parents:
+                    raise ValueError("Tar file contents move upwards past sandbox root")
+
             tar.extractall(path=cmake_local_dir)
 
     return cmake_local_exe
 
 
-def get_ninja(download, verbose):
+def _get_ninja(download: bool, verbose: bool) -> pathlib.Path:
     """Return the path to system Ninja, or download and unpack a local copy."""
     if not download:
-        ninja = shutil.which('ninja')
+        ninja = shutil.which("ninja")
         if ninja:
-            return ninja
+            return pathlib.Path(ninja)
 
-    ninja_local_dir = SCRIPT_PATH / 'external' / 'ninja'
-    plat = {'darwin': 'mac', 'linux': 'linux', 'win32': 'win'}[sys.platform]
-    ninja_file = f'ninja-{plat}.zip'
+    ninja_local_dir = _SCRIPT_PATH / "external/ninja"
+    plat = {"darwin": "mac", "linux": "linux", "win32": "win"}[sys.platform]
+    ninja_file = f"ninja-{plat}.zip"
     ninja_local_zip = ninja_local_dir / ninja_file
-    ninja_local_exe = ninja_local_dir / f'ninja{".exe" if plat == "win" else ""}'
+    ninja_local_exe = ninja_local_dir / f"ninja{'.exe' if plat == 'win' else ''}"
 
     if not ninja_local_exe.exists():
         if not ninja_local_zip.exists():
             ninja_local_dir.mkdir(parents=True, exist_ok=True)
-            download_file(
-                NINJA_URL.format(ninja_file),
-                ninja_local_zip,
-                verbose)
-        with zipfile.ZipFile(ninja_local_zip, 'r') as zip_file:
+            download_file(_NINJA_URL.format(ninja_file), ninja_local_zip, verbose)
+
+        with zipfile.ZipFile(ninja_local_zip, "r") as zip_file:
             zip_file.extractall(ninja_local_dir)
-        os.chmod(ninja_local_exe, os.stat(
-            ninja_local_exe).st_mode | stat.S_IEXEC)
+
+        os.chmod(ninja_local_exe, os.stat(ninja_local_exe).st_mode | stat.S_IEXEC)
 
     return ninja_local_exe
 
 
-def configure_cmake(cmake_exe, ninja, args):
+def _configure_cmake(
+    cmake_exe: pathlib.Path, ninja: pathlib.Path, args: argparse.Namespace
+) -> bool:
     """Prepare CMake for building nanoprintf tests under 'build/ninja/<cfg>'."""
-    build_path = SCRIPT_PATH / 'build' / 'ninja' / args.cfg
-    if (build_path / 'CMakeFiles').exists():
+    build_path = _SCRIPT_PATH / "build/ninja" / args.cfg
+    if (build_path / "CMakeFiles").exists():
         return True
 
     sys.stdout.flush()
     build_path.mkdir(parents=True)
 
-    cmake_args = [cmake_exe,
-                  SCRIPT_PATH,
-                  '-G',
-                  'Ninja',
-                  '-DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
-                  f'-DCMAKE_MAKE_PROGRAM={ninja}',
-                  f'-DCMAKE_BUILD_TYPE={args.cfg}',
-                  f'-DNPF_PALAND={"ON" if args.paland else "OFF"}',
-                  f'-DNPF_32BIT={"ON" if args.arch == 32 else "OFF"}',
-                  f'-DNPF_CLANG_ASAN={"ON" if args.asan else "OFF"}',
-                  f'-DNPF_CLANG_UBSAN={"ON" if args.ubsan else "OFF"}']
+    cmake_args = [
+        cmake_exe,
+        _SCRIPT_PATH,
+        "-G",
+        "Ninja",
+        "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
+        f"-DCMAKE_MAKE_PROGRAM={ninja}",
+        f"-DCMAKE_BUILD_TYPE={args.cfg}",
+        f"-DNPF_PALAND={'ON' if args.paland else 'OFF'}",
+        f"-DNPF_32BIT={'ON' if args.arch == 32 else 'OFF'}",
+        f"-DNPF_CLANG_ASAN={'ON' if args.asan else 'OFF'}",
+        f"-DNPF_CLANG_UBSAN={'ON' if args.ubsan else 'OFF'}",
+    ]
+
     try:
-        return subprocess.run(
-            cmake_args,
-            cwd=build_path,
-            check=True).returncode == 0
+        return subprocess.run(cmake_args, cwd=build_path, check=True).returncode == 0
     except subprocess.CalledProcessError as cpe:
         return cpe.returncode == 0
 
 
-def build_cmake(cmake_exe, args):
+def _build_cmake(cmake_exe: pathlib.Path, args: argparse.Namespace) -> bool:
     """Run CMake in build mode to compile and run the nanoprintf test suite."""
     sys.stdout.flush()
-    build_path = SCRIPT_PATH / 'build' / 'ninja' / args.cfg
-    cmake_args = [cmake_exe, '--build', build_path] + \
-        (['--', '-v'] if args.verbose else [])
+    build_path = _SCRIPT_PATH / "build/ninja" / args.cfg
+    cmake_args = [cmake_exe, "--build", build_path] + (
+        ["--", "-v"] if args.verbose else []
+    )
+
     try:
         return subprocess.run(cmake_args, check=True).returncode == 0
     except subprocess.CalledProcessError as cpe:
         return cpe.returncode == 0
 
 
-def main():
+def main() -> int:
     """Parse args, find or get tools, configure CMake, build and run tests."""
-    args = parse_args()
+    args = _parse_args()
 
-    cmake = get_cmake(args.download, args.verbose)
+    cmake = _get_cmake(args.download, args.verbose)
     if args.verbose:
-        print(f'Found CMake at {cmake}')
+        print(f"Found CMake at {cmake}")
 
-    ninja = get_ninja(args.download, args.verbose)
+    ninja = _get_ninja(args.download, args.verbose)
     if args.verbose:
-        print(f'Found ninja at {ninja}')
+        print(f"Found ninja at {ninja}")
 
-    built_ok = configure_cmake(cmake, ninja, args) and build_cmake(cmake, args)
+    built_ok = _configure_cmake(cmake, ninja, args) and _build_cmake(cmake, args)
     return int(not built_ok)  # 0 is success
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
