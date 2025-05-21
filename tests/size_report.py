3bb877414deb2e48f60d8e3c756a9549233011d0
Charles Nicholson <charles.nicholson@gmail.com>
2025-05-21 07:55:04 -0400

    avr2, avr5

--- tests/size_report.py
+++ tests/size_report.py
@@ -14,7 +14,7 @@ def _parse_args() -> argparse.Namespace:
         "-p",
         "--platform",
         required=True,
-        choices=("cm0", "cm4", "avr", "host"),
+        choices=("cm0", "cm4", "avr2", "avr5", "host"),
         help="target platform",
     )
     return parser.parse_args()
@@ -33,28 +33,45 @@ def _git_root() -> pathlib.Path:
 
 def _build(platform: str, flags: list[str]) -> str:
     """Build a nanoprintf implementation object for platform + flags."""
-    cc_exe = {
-        "cm0": "arm-none-eabi-gcc",
-        "cm4": "arm-none-eabi-gcc",
-        "avr": "avr-gcc",
-        "host": "cc",
-    }[platform]
 
-    cc_cmd = [cc_exe, "-c", "-x", "c", "-Os", f"-I{_git_root()}", "-o", "npf.o"]
-    cc_cmd += {
-        "cm0": ["-mcpu=cortex-m0"],
-        "cm4": ["-mcpu=cortex-m4", "-mfloat-abi=hard"],
-        "avr": [],
-        "host": [],
-    }[platform]
-    cc_cmd += ["-DNANOPRINTF_IMPLEMENTATION"] + flags + ["-"]
+    cc_cmd = []
 
-    nm_exe = {
-        "cm0": "arm-none-eabi-nm",
-        "cm4": "arm-none-eabi-nm",
-        "avr": "avr-nm",
-        "host": "nm",
-    }[platform]
+    match platform:
+        case "host":
+            cc_exe = "cc"
+            nm_exe = "nm"
+
+        case _ if platform.startswith("cm"):
+            cc_exe = "arm-none-eabi-gcc"
+            cc_cmd += [f"-mcpu=cortex-m{platform[-1]}"]
+
+            if platform == "cm4":
+                cc_cmd += ["-mfloat-abi=hard"]
+
+            nm_exe = "arm-none-eabi-nm"
+
+        case _ if platform.startswith("avr"):
+            cc_exe = "avr-gcc"
+            cc_cmd += [f"-mmcu={platform}"]
+            nm_exe = "avr-nm"
+
+        case unknown:
+            msg = f"Unknown platform {unknown}"
+            raise ValueError(msg)
+
+    cc_cmd.insert(0, cc_exe)
+    cc_cmd += [
+        "-c",
+        "-x",
+        "c",
+        "-Os",
+        f"-I{_git_root()}",
+        "-o",
+        "npf.o",
+        "-DNANOPRINTF_IMPLEMENTATION",
+        *flags,
+        "-",
+    ]
 
     nm_cmd = [nm_exe, "--print-size", "--size-sort", "npf.o"]
 
@@ -74,15 +91,19 @@ def _measure(build_output: str) -> None:
     """Parse the results of nm to accumulate and print a total size."""
     total = 0
     for line in build_output.split("\n"):
-        parts = [l for l in line.split() if l.strip()]
-        if not parts:
+        if not (parts := [x for x in line.split() if x.strip()]):
             continue
+
         print(line)
+
         if parts[0] in ("u", "U"):
             continue
+
         if len(parts) >= 3 and parts[2] not in ("t", "T"):
             continue
+
         total += int(line.split()[1], 16)
+
     print(f"Total size: 0x{total:x} ({total}) bytes")
 
 
@@ -174,6 +195,7 @@ def main() -> int:
         print(f'Configuration "{cfg["name"]}":')
         _measure(_build(_parse_args().platform, cfg["flags"]))
         print()
+
     return 0
 
 
